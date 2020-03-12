967714ee324acb8d422b0f6ce1f7c583da6879e2
Hartmann <alemaohartmann@gmail.com>
2020-03-11 19:50:21 -0500

    :rocket: Utilizando Slug para mudar a forma de associação de id para nome da categoria em Publicacao

--- tutorialpress/tutorialpress/core/serializers/publicacao.py
+++ tutorialpress/tutorialpress/core/serializers/publicacao.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from tutorialpress.core.models import Publicacao
+from tutorialpress.core.models import Categoria, Publicacao
 
 
 class PublicacaoSerializer(serializers.ModelSerializer):
+    categoria = serializers.SlugRelatedField(default=None, queryset=Categoria.objects.all(), slug_field="nome")
+
     class Meta:
         model = Publicacao
         fields = ["id", "titulo", "conteudo", "is_publicada", "categoria"]
+
+
+class PublicacaoDetailSerializer(PublicacaoSerializer):
+    categoria = serializers.CharField(source="categoria.nome", default=None)
--- tutorialpress/tutorialpress/core/views/publicacao.py
+++ tutorialpress/tutorialpress/core/views/publicacao.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 
 from tutorialpress.core.models import Publicacao
-from tutorialpress.core.serializers import PublicacaoSerializer
+from tutorialpress.core.serializers import PublicacaoDetailSerializer, PublicacaoSerializer
 
 
 class PublicacaoViewSet(viewsets.ModelViewSet):
     queryset = Publicacao.objects.all()
-    serializer_class = PublicacaoSerializer
+    lookup_field = "id"
+    # serializer_class = PublicacaoSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        if self.action == "list":
+            return PublicacaoDetailSerializer(*args, **kwargs)
+        else:
+            return PublicacaoSerializer(*args, **kwargs)
